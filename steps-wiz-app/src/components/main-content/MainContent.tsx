4b299170e29fdb851165c9455a7a1d29037cc875
Mazzzy <shaikhmazahar@gmail.com>
2020-11-12 19:39:05 +0530

    implemented 5 textareas for corresponding steps

--- steps-wiz-app/src/components/main-content/MainContent.tsx
+++ steps-wiz-app/src/components/main-content/MainContent.tsx
@@ -5,10 +5,28 @@ import "./MainContent.css";
 
 const MainContent: FC = () => {
     const [val, setVal] = useState("");
+    const steps = [
+        { id: "1", title: "Examination" },
+        { id: "2", title: "Clinical History" },
+        { id: "3", title: "Technique" },
+        { id: "4", title: "Findings" },
+        { id: "5", title: "Impressions" },
+    ];
+
+    const txtChangeHandler = (e: string) => {
+        console.log("On step ", e);
+    };
+
+    const stepsCollection = steps.map((elem, index) => [
+        <div key={`step-container-${index}`}>
+            <h3>{elem?.title || ""}</h3>
+            <TextField placeholder={elem?.title} value={val} onChange={txtChangeHandler} textarea />
+        </div>,
+    ]);
+
     return (
         <div className="main-contents">
-            <p>Main contents goes here</p>
-            <TextField placeholder="Input box" value={val} onChange={setVal} textarea />
+            {stepsCollection.length === 0 ? <p>No Steps data available</p> : stepsCollection}
         </div>
     );
 };

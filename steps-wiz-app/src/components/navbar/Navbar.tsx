79e92316aa73b3e31f0d6efbbef06997c7f0d2c5
Mazzzy <shaikhmazahar@gmail.com>
2020-11-12 17:52:59 +0530

    added basic navbar and main content

--- steps-wiz-app/src/components/navbar/Navbar.tsx
+++ steps-wiz-app/src/components/navbar/Navbar.tsx
@@ -1,15 +1,24 @@
-import React, { FC, MouseEvent } from "react";
+import React, { FC } from "react";
+import defaultLogo from "./default-logo.svg";
+
 import "./Navbar.css";
 
 const Navbar: FC = () => {
     return (
         <header>
             <nav className="navbar" role="navigation" aria-label="main navigation">
-                <a>Brand</a>
-                <ul>
-                    <li>Menu1</li>
-                    <li>Menu 2</li>
-                </ul>
+                <div className="navbar-brand">
+                    <a className="navbar-item" href="#">
+                        <img className="logo-img" src={defaultLogo} alt="logo-img" />
+                    </a>
+                </div>
+                <div className="navbar-menu">
+                    <div className="navbar-end">
+                        <a className="navbar-item is-active" href="#">
+                            Home
+                        </a>
+                    </div>
+                </div>
             </nav>
         </header>
     );
